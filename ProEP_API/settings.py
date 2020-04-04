1d4a3fbf39d16bdbecf6f185c60e0fc727340341
Dimitar Dyakov <dimitar.dyakov98@gmail.com>
2020-04-04 13:44:49 +0200

    added back database configs

--- ProEP_API/settings.py
+++ ProEP_API/settings.py
@@ -51,12 +51,12 @@ TEMPLATES = [
 WSGI_APPLICATION = 'ProEP_API.wsgi.application'
 
 # We will configure that later with PostgreSQL
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 AUTH_PASSWORD_VALIDATORS = [
     {
