e62645b57708cbf9b305b7d103ff88e6f7becc1e
Dimitar Dyakov <dimitar.dyakov98@gmail.com>
2020-04-18 18:35:51 +0200

    changed debug value

--- ProEP_API/settings.py
+++ ProEP_API/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-DEBUG = (os.environ.get('DEBUG') == 'True')
+DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'xplore-backend-production.herokuapp.com', 'xplore-backend-staging.herokuapp.com']
 
