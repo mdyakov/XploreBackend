439f9e4744462e9895a516136dbd8ad5b3311284
Dimitar Dyakov <dimitar.dyakov98@gmail.com>
2020-04-18 18:24:46 +0200

    removed config line

--- ProEP_API/settings.py
+++ ProEP_API/settings.py
@@ -66,9 +66,6 @@ DATABASES = {
     }
 }
 
-if not DEBUG:
-    DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -85,8 +82,6 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 LANGUAGE_CODE = 'en-us'
 
