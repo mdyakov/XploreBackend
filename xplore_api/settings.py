4fe76c5c9952a03de5bf0e42fe167f1f4c88e619
Dimitar Dyakov <dimitar.dyakov98@gmail.com>
2020-05-01 15:43:41 +0200

    added empty lines

--- xplore_api/settings.py
+++ xplore_api/settings.py
@@ -22,10 +22,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework'
 ]
+
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
