711e77207cb9a215c71653cf30c2215d895f5823
Abdirimov,Maruf M <m.abdirimov@student.fontys.nl>
2020-05-01 15:17:43 +0200

    Added urls for login and logout. Logout isn't working yet.

--- ProEP_API/urls.py
+++ ProEP_API/urls.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from users_API import views as user_views
+from rest_framework.authtoken.views import obtain_auth_token
+
+router = routers.DefaultRouter()
+router.register(r'users', user_views.UserViewSet, basename='user')
+router.register(r'groups', user_views.GroupViewSet)
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('admin/', admin.site.urls),
+    # path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('login/', obtain_auth_token, name='api_token_auth'),  # generating token if correct username and password combination has been picked 
+    path('logout/', user_views.logout)
 ]
