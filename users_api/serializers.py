6725ffdaa23cd705f5cf4c3005907eded337c098
Abdirimov,Maruf M <m.abdirimov@student.fontys.nl>
2020-05-02 02:56:54 +0200

    Changes are done to make sure UserViewSet works with username rather than id

--- users_api/serializers.py
+++ users_api/serializers.py
@@ -3,6 +3,11 @@ from rest_framework import serializers
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
+    url = serializers.HyperlinkedIdentityField( ## Required for retrieving Users based on Username rather than id
+        view_name='user-detail',
+        lookup_field='username'
+    ) 
+
     class Meta:
         model = User
         fields = ['url', 'username', 'email', 'groups']
