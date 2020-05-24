798ba5087b1da382aa3e60266d6aaab4d23b4e96
Abdirimov,Maruf M <m.abdirimov@student.fontys.nl>
2020-05-24 17:22:16 +0200

    Wishlist and favorites endpoints have been implemented

---
+++ users_api/permissions.py
@@ -0,0 +1,26 @@
+
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
+from rest_framework import permissions
+
+class IsOwner(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return super().has_permission(request, view)
+
+    def has_object_permission(self, request, view, obj):
+        print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!", obj)
+        if request.user == obj:
+            return True
+        else:
+            return False
+        # if :
+        #     return True
+ 
+        # w_u = request.user.websiteuser_set.filter(user_id=request.user.id, email_settings_id=obj.id).first()
+        # if w_u:
+        #     return True
+        # else:
+        #     if request.method.lower() == 'get':
+        #         return True
+ 
+        # return False
--- users_api/views.py
+++ users_api/views.py
@@ -4,11 +4,14 @@ from django.shortcuts import get_object_or_404
 from rest_framework import viewsets, permissions, authentication, status
 from rest_framework.decorators import action, api_view, renderer_classes
 from rest_framework.response import Response
-from users_api.serializers import UserSerializer
+from users_api.serializers import UserSerializer, GameSerializer, WishlistSerializer, FavoritesSerializer
 from django.core.exceptions import ObjectDoesNotExist
 from rest_framework.renderers import JSONRenderer, TemplateHTMLRenderer
 from rest_framework.authtoken.models import Token
 from django.http import HttpResponse, JsonResponse
+from .permissions import IsOwner
+from .models import Game, Favorites, Wishlist
+
 
 class UserViewSet(viewsets.ModelViewSet):
     """
@@ -17,7 +20,7 @@ class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
     queryset = User.objects.all()
     lookup_field = 'username'
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [IsOwner]  # [permissions.IsAuthenticated, IsOwner]
     authentication_classes = (authentication.TokenAuthentication,)
     @action(detail=False, methods=['POST'], url_path='logout', url_name='logout')
     def logout(self, request):
@@ -27,7 +30,7 @@ class UserViewSet(viewsets.ModelViewSet):
             pass
         return Response(data={"success": "Successfully logged out."},
                         status=status.HTTP_200_OK)
-        
+
     @action(detail=False, methods=['GET'], url_path='me', url_name='me')
     def get_token_user(self, request):
         # print(request.user)
@@ -37,6 +40,57 @@ class UserViewSet(viewsets.ModelViewSet):
         user = Token.objects.get(key=request.user.auth_token).user
         print(user)
         return JsonResponse(UserSerializer(instance=user, context=serializer_context).data, safe=False)
+
+    @action(detail=True, methods=['GET', 'POST', 'DELETE'], url_path='wishlist', url_name='wishlist')
+    def wishlist(self, request, username=None):
+        if request.user.username != username:
+            return Response(data={"detail": "You do not have permission to perform this action."}, status=status.HTTP_403_FORBIDDEN)
+
+        user = User.objects.get(username=username)
+        wishlist = Wishlist.objects.get(user=user)
+        serializer_context = {
+            'request': request,
+        }
+        print(request.method)
+        if request.method == 'POST':
+            game, exists = Game.objects.get_or_create(id=request.data['id'],
+                                                  name=request.data['name'],
+                                                  poster_url=request.data['poster_url'])
+            wishlist.games.add(game)
+        elif request.method == 'DELETE':
+            game = Game.objects.get(id=request.data['id'])
+            wishlist.games.remove(game)
+            wishlist.save()
+        wishlist = Wishlist.objects.get(user=user)
+        print(wishlist.games)
+        return JsonResponse(WishlistSerializer(instance=wishlist, context=serializer_context).data, safe=False)
+
+    @action(detail=True, methods=['GET', 'POST', 'DELETE'], url_path='favorites', url_name='favorites')
+    def get_favorites(self, request, username=None):
+        if request.user.username != username:
+            return Response(data={"detail": "You do not have permission to perform this action."}, status=status.HTTP_403_FORBIDDEN)
+
+        user = User.objects.get(username=username)
+        favorites = Favorites.objects.get(user=user)
+        serializer_context = {
+            'request': request,
+        }
+        print(request.method)
+        if request.method == 'POST':
+            game, exists = Game.objects.get_or_create(id=request.data['id'],
+                                                  name=request.data['name'],
+                                                  poster_url=request.data['poster_url'])
+            print(game)
+            favorites.games.add(game)
+        elif request.method == 'DELETE':
+            game = Game.objects.get(id=request.data['id'])
+            favorites.games.remove(game)
+            favorites.save()
+        favorites = Favorites.objects.get(user=user)
+        print(favorites.games)
+        Favorites
+        return JsonResponse(FavoritesSerializer(instance=favorites, context=serializer_context).data, safe=False)
+
     def get_permissions(self):
         """
         Instantiates and returns the list of permissions that this view requires.
@@ -44,5 +98,5 @@ class UserViewSet(viewsets.ModelViewSet):
         if self.action == 'create':
             permission_classes = [permissions.AllowAny]
         else:
-            permission_classes = [permissions.IsAuthenticated]
+            permission_classes = [IsOwner]
         return [permission() for permission in permission_classes]
